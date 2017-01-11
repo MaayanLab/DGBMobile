c840f51390902869c902c1418ee27028fcf1f654
Edward He <edward.he@macaulay.cuny.edu>
2017-01-11 13:03:53 -0500

    Create scaffolding of HomeScreen.

--- app/HomeScreen/index.js
+++ app/HomeScreen/index.js
@@ -4,19 +4,35 @@ import {
   StyleSheet,
   Text,
   View,
+  Button,
 } from 'react-native';
 
 export default class HomeScreen extends Component {
   static route = {
     navigationBar: {
-      title: 'Home',
+      title: 'Dr. Gene Badger',
     }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  _submitForm() {
+    // Make post request here based on
+    // state
   }
 
   render() {
     return (
       <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-        <Text>HomeScreen!</Text>
+        <Text>Gene Name</Text>
+        <Text>Expression</Text>
+        <Text>Dataset</Text>
+        <Button title="Submit" onPress={() => console.log("Hello")}/>
       </View>
     )
   }
